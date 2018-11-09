ba7f1be72c7cb100d9703e6719dc671d17cfa1f9
rnanclares <raulnanclares@gmail.com>
2018-11-09 16:08:15 -0600

    Curso PyQGIS primer dia

--- palindromo/palidromo.py
+++ palindromo/palidromo.py
@@ -1,7 +1,11 @@
+import re
 
 def checkpali(a):
-    a = a.lower().replace(' ','')
-    if a[::-1] == a:
+    regex = re.compile('[^a-zA-Z]')
+    b = a
+    b = b.lower().replace(' ','')
+    b = regex.sub('', b)
+    if b[::-1] == b:
         print('{} es un palíndromo.'.format(a))
     else:
         print('{} no es un palíndromo.'.format(a))
